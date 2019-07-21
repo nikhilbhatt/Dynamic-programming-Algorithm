4b5d9bbfd189ece8614216749dca9d65ff2a92b9
Nikhil Bhatt <nikhilbhatt931@gmail.com>
2019-07-21 14:30:51 +0530

    updated Knapsack

--- 0_1Knapsack problem[O(nwt)].cpp
+++ 0_1Knapsack problem[O(nwt)].cpp
@@ -22,7 +22,8 @@ int main()
 }
 void knapsack(vector<int> &val,vector<int> &w,int wt)
 {
-    int i,j,temp,a[val.size()+2][wt+2];
+    //condition:- weight should be in sorted manner with values.
+    int i,j,temp,a[val.size()+1][wt+1];  // memoization table
     for(i=0;i<=val.size();i++)
     {
         for(j=0;j<=wt;j++)
@@ -32,11 +33,38 @@ void knapsack(vector<int> &val,vector<int> &w,int wt)
             else
               if(w.at(i-1)<=j)
                  a[i][j]=a[i-1][j]>(val.at(i-1)+a[i-1][j-w.at(i-1)])?a[i-1][j]:(val.at(i-1)+a[i-1][j-w.at(i-1)]);
+                   //val.at(i-1)+a[i-1][j-w.at(i-1)]=effect of adding the new value+ effect of remaining sum value)
+                   //max(a[i-1][j],val.at(i-1)+a[i-1][j-w.at(i-1)]);
+                   //updating value of i,j using previous values.
            else
             a[i][j]=a[i-1][j];
 
         }
     }
-    cout<<a[val.size()][wt];
+    cout<<a[val.size()][wt]<<endl; //final value of 2d array i.e.Table
+    int weight[val.size()+1]={0},sx=0;
+    temp=a[val.size()][wt];
+    for(i=val.size()-1;i>=0;i--)
+    {
+        sx=0;
+        for(j=0;j<=wt;j++)
+        {
+            if(a[i][j]==temp)
+            {
+                sx=1;
+                break;
+            }
+        }
+        if(sx==0)
+        {
+            weight[i]=1;
+            temp=temp-val.at(i);
+        }
+        //if(temp<0)
+            //break;
+
+    }
+    for(i=0;i<val.size();i++)
+        cout<<weight[i]<<" ";
 }
 
